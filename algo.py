6969ac86bf31d45992c49ada4d2349f25e853bf1
Fernando1929 <luis.quiles1@upr.edu>
2020-11-11 21:14:36 -0400

    Initial commit basic algorithm

---
+++ algo.py
@@ -0,0 +1,77 @@
+from bst import Node
+import json
+
+#Another issue do we use the leaders working hours or persons working hours 
+
+#Utils (Maybe a class later) Not used for now
+days = ['Monday','Thuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+
+####First: we get the data
+#working period of the person
+working_hours_start = 10
+working_hours_end = 17
+mean = 10 + (working_hours_end - working_hours_start)/2
+
+#Period of for looking for a free slot (Basically if looking for a meeting in a week period is 7)
+period = 7
+
+# Now we need the persons schedule
+# get appointments in the json data
+schedule = [[]for i in range(period)]
+with open('./data.txt') as json_file:
+    data = json.load(json_file)
+    for e in data['data']['schedule']:
+        event = e['start_date_time'][-14:]
+        day = e['start_date_time'][-14:][0]
+        if day == ("M"):
+                schedule[0].append(int(event[1:3]))
+        if day == ("T"):
+                schedule[1].append(int(event[1:3]))
+        if day == ("W"):
+                schedule[2].append(int(event[1:3]))
+        if day == ("R"):
+                schedule[3].append(int(event[1:3]))
+        if day == ("F"):
+                schedule[4].append(int(event[1:3]))
+        if day == ("S"):
+                schedule[5].append(int(event[1:3]))
+        if day == ("U"):
+                schedule[6].append(int(event[1:3]))
+
+#print the array with the schedule        
+#print(schedule)
+
+# Gets the free slots of time based on the period or working_hours_start and working_hours_end
+def getFreeTimeSlots():
+    result = [[]for i in range(period)]
+    start = working_hours_start
+    for i in range(period):
+        while(start< working_hours_end):
+            if not schedule[i].count(start) > 0:
+                result[i].append(start)
+            start += 1
+        start = working_hours_start
+    return result
+freeSlot = getFreeTimeSlots()
+
+# This is the schedule of the person
+# This is not needed is just the fake appointments for testing
+#search_busySchedule = [Node(mean) for i in range(period)]
+
+#This is the tree with the free slots of time
+search_freeSchedule = [Node(mean) for i in range(period)]
+
+###Second: we put it in the tree
+# for i in range(period):
+#     for j in range(len(schedule[i])):
+#         search_busySchedule[i] = Node.insert(search_busySchedule[i],schedule[i][j])
+
+for i in range(period):
+    for j in range(len(freeSlot[i])):
+        search_freeSchedule[i] = Node.insert(search_freeSchedule[i],freeSlot[i][j])
+
+# Print the tree
+print()
+for i in range(period):
+    print("tree of", days[i])
+    Node.inorder( search_freeSchedule[i])
---
+++ bst.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+    
+    # A utility function to insert 
+    # a new node with the given key
+    
+    
+    def insert(root, key):
+        if root is None:
+            return Node(key)
+        else:
+            if root.val == key:
+                return root
+            elif root.val < key:
+                root.right = Node.insert(root.right, key)
+            else:
+                root.left = Node.insert(root.left, key)
+        return root
+    
+    # A utility function to do inorder tree traversal
+    
+    
+    def inorder(root):
+        if root:
+            Node.inorder(root.left)
+            print(root.val)
+            Node.inorder(root.right)
+    
+    def search(root,key):
+        # Base Cases: root is null or key is present at root
+        if root is None or root.val == key:
+            return root
+    
+        # Key is greater than root's key
+        if root.val < key:
+            return Node.search(root.right,key)
+    
+        # Key is smaller than root's key
+        return Node.search(root.left,key)
+ 
