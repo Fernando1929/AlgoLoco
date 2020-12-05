e2ac7ba61b961a700eae984fe7df1f4367bcaa45
Luis Fernando Quiles Ruiz <luis.quiles1@upr.edu>
2020-12-05 17:46:57 -0400

    Refactor some code

--- algo.py
+++ algo.py
@@ -1,68 +1,47 @@
 from bst import Node
 import json
 import timeit
-#Another issue do we use the leaders working hours or persons working hours 
 
-#Utils (Maybe a class later) Not used for now
-days = ['Monday','Thuesday','Wednesday','Thursday','Friday','Saturday','Sunday']
+# Data from the DB
+leaderScheduleFile = "./leaderschedule.txt"
+memeberScheduleFile =  "./memberschedule.txt"
 
-####First: we get the data
-#working period of the person
+# Params from the DB data
+# working period of the person
 working_hours_start = 10
 working_hours_end = 17
-mean = 10 + (working_hours_end - working_hours_start)/2
+mean = 10 + (working_hours_end - working_hours_start)/2 # Help balance the trees 
 
-#Period of for looking for a free slot (Basically if looking for a meeting in a week period is 7)
+#Period for looking for a free slot (Basically if looking for a meeting in a week period is 7)
 period = 7
 
 # Now we need the persons schedule
 # get appointments in the json data
-leader_schedule = [[]for i in range(period)]
-with open('./leaderschedule.txt') as json_file:
-    data = json.load(json_file)
+def readSchedule(file):
+    schedule = [[]for i in range(period)]
+    with open(file) as json_file:
+        data = json.load(json_file)
     for e in data['data']['schedule']:
-        event = e['start_date_time'][-14:]
-        day = e['start_date_time'][-14:][0]
-        if day == ("M"):
-                leader_schedule[0].append(int(event[1:3]))
-        if day == ("T"):
-                leader_schedule[1].append(int(event[1:3]))
-        if day == ("W"):
-                leader_schedule[2].append(int(event[1:3]))
-        if day == ("R"):
-                leader_schedule[3].append(int(event[1:3]))
-        if day == ("F"):
-                leader_schedule[4].append(int(event[1:3]))
-        if day == ("S"):
-                leader_schedule[5].append(int(event[1:3]))
-        if day == ("U"):
-                leader_schedule[6].append(int(event[1:3]))
+            event = e['start_date_time'][-14:]
+            day = e['start_date_time'][-14:][0]
+            if day == ("M"):
+                    schedule[0].append(int(event[1:3]))
+            if day == ("T"):
+                    schedule[1].append(int(event[1:3]))
+            if day == ("W"):
+                    schedule[2].append(int(event[1:3]))
+            if day == ("R"):
+                    schedule[3].append(int(event[1:3]))
+            if day == ("F"):
+                    schedule[4].append(int(event[1:3]))
+            if day == ("S"):
+                    schedule[5].append(int(event[1:3]))
+            if day == ("U"):
+                    schedule[6].append(int(event[1:3]))
+    return schedule
 
-member_schedule = [[]for i in range(period)]
-with open('./memberschedule.txt') as json_file:
-    data = json.load(json_file)
-    for e in data['data']['schedule']:
-        event = e['start_date_time'][-14:]
-        day = e['start_date_time'][-14:][0]
-        if day == ("M"):
-                member_schedule[0].append(int(event[1:3]))
-        if day == ("T"):
-                member_schedule[1].append(int(event[1:3]))
-        if day == ("W"):
-                member_schedule[2].append(int(event[1:3]))
-        if day == ("R"):
-                member_schedule[3].append(int(event[1:3]))
-        if day == ("F"):
-                member_schedule[4].append(int(event[1:3]))
-        if day == ("S"):
-                member_schedule[5].append(int(event[1:3]))
-        if day == ("U"):
-                member_schedule[6].append(int(event[1:3]))
-
-#print the array with the schedule        
-#print(schedule)
-
-# Gets the free slots of time based on the period or working_hours_start and working_hours_end
+#Gets the free slots of time based on the period 
+# or working_hours_start and working_hours_end
 def getFreeTimeSlots(ws,we,s):
     result = [[]for i in range(period)]
     start = ws
@@ -74,6 +53,8 @@ def getFreeTimeSlots(ws,we,s):
         start = ws
     return result
 
+leader_schedule = readSchedule(leaderScheduleFile)
+member_schedule = readSchedule(memeberScheduleFile)
 leader_freeSlots = getFreeTimeSlots(working_hours_start,working_hours_end, leader_schedule)
 member_freeSlots = getFreeTimeSlots(working_hours_start,working_hours_end, member_schedule)
 
@@ -98,7 +79,6 @@ for i in range(period):
     for j in range(len(member_freeSlots[i])):
         member_searchFreeSchedule[i] = Node.insert(member_searchFreeSchedule[i],member_freeSlots[i][j])
 
-
 # a and b are array of bsts 
 def getFreeHoursInCommon1(a,b):
         result = [[]for i in range(period)]
@@ -106,17 +86,11 @@ def getFreeHoursInCommon1(a,b):
         #leader = [[12, 13, 13.5], [10, 13.5, 15], [13.5, 15], [13.5, 14], [11, 13.5], [13.5], [13.5]] #with time in common 
         #print(leader)
         for i in range(len(leader)):
-                for j in range(len(leader[i])):
-                        if Node.search(b[i],leader[i][j]) != None and Node.search(b[i],leader[i][j]) != b[i] :
-                                result[i].append(leader[i][j]) 
-
+            for j in range(len(leader[i])):
+                if Node.search(b[i],leader[i][j]) != None and Node.search(b[i],leader[i][j]) != b[i] :
+                    result[i].append(leader[i][j]) 
         return result
         
-s1 = timeit.default_timer()
-print(getFreeHoursInCommon1(leader_searchFreeSchedule,member_searchFreeSchedule))
-s2 = timeit.default_timer()
-print('Time with loops: ', s2 - s1)  
-
 #receive the leader root of the day and member root of the day returns the array of slots in common
 def inordersearch(lroot, mroot):
         arr = []
@@ -134,22 +108,17 @@ def getFreeHoursInCommon(a,b):
                 result[i] = inordersearch(a[i],b[i])
         return result
 
+## Time Recursion
 start = timeit.default_timer() 
 print(getFreeHoursInCommon(leader_searchFreeSchedule,member_searchFreeSchedule))
 stop = timeit.default_timer()
-
 print('Time with recursion: ', stop - start)  
 
-# for i in range(period):
-#     print("leader")
-#     print("tree of", days[i])
-#     Node.inorder(leader_searchFreeSchedule[i])
-#     print("member")
-#     print("tree of", days[i])
-#     Node.inorder(member_searchFreeSchedule[i])
-#     print(res[i])
-
-
+## Time Loops
+s1 = timeit.default_timer()
+print(getFreeHoursInCommon1(leader_searchFreeSchedule,member_searchFreeSchedule))
+s2 = timeit.default_timer()
+print('Time with loops: ', s2 - s1)  
 
 
 
